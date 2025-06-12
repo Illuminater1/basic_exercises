e6391dc89ae605f6820ae77ee72f4275afa6f75d
Nikolay R <romashcn@gmail.com>
2025-06-13 00:49:09 +0300

    Пятое задание как будто не совсем так

--- for_dict_challenges_bonus.py
+++ for_dict_challenges_bonus.py
@@ -33,6 +33,7 @@ messages = [
 import random
 import uuid
 import datetime
+from collections import defaultdict
 
 import lorem
 
@@ -66,5 +67,106 @@ def generate_chat_history():
     return messages
 
 
+def wrote_the_most_msg(messages):
+    '''
+    Какой пользователь отправил больше всего сообщений
+    '''
+    msg_counter = defaultdict(int)
+    for msg in messages:
+        msg_counter[msg["sent_by"]] += 1
+    result =  max(msg_counter.items(),
+               key=lambda v: v[1])
+    return f"Пользователь с id {result[0]} отправил больше всего сообщений - {result[1]}"
+
+
+def most_quoted_user(messages):
+    '''
+    Какому пользователю больше всего отвечали
+    '''
+    counter = defaultdict(int)
+    for msg in messages:
+        if not msg["reply_for"] is None:
+            msg_id = msg["id"]
+            for message in messages:
+                if message["id"] == msg_id:
+                    counter[message["sent_by"]] += 1
+    result = max(counter.items(),
+                 key=lambda v: v[1])
+    return f"Пользователю с id {result[0]} отвечали больше всего раз - {result[1]}"
+
+
+def most_readed_user(messages):
+    '''
+    Сообщения какого юзера читали больше всего
+    '''
+    counter = {}
+    for msg in messages:
+        counter.setdefault(msg["sent_by"], []).extend(msg["seen_by"])
+
+    result = max(counter.items(),
+                 key=lambda v: len(set(v[1])))
+    return f"Сообщения пользователя с id {result[0]} читали больше всего уникальных пользователей - {len(set(result[1]))}"
+
+
+def more_popular_time(messages):
+    #когда отправляется больше всего сообщений
+    counter = {"утром": 0,
+               "днем": 0,
+               "вечером": 0,
+               }
+    for msg in messages:
+        time = msg["sent_at"].time()
+        if time < datetime.time(hour=12):
+            counter["утром"] += 1
+        elif time < datetime.time(hour=18):
+            counter["днем"] += 1
+        else:
+            counter["вечером"] += 1
+    result = max(counter.items(),
+                 key=lambda v: v[1])
+    return f"Больше всего сообщений - {result[1]} отправляется {result[0]}"
+
+
+
+def is_reply_for(messages, id, lk_count):
+    for msg in messages:
+        if msg["id"] == id:
+            if msg["reply_for"] is None:
+                return (lk_count, id)
+            else:
+                lk_count += 1
+                id = msg["reply_for"]
+                return is_reply_for(messages, id, lk_count)
+
+
+
+def long_trade(messages):
+    '''
+    Какое сообщение начало самый длинный трейд
+    '''
+    gl_count = 0
+    msg_id = ""
+    for msg in messages:
+        if msg["reply_for"] is not None:
+            tmp = is_reply_for(messages, msg["reply_for"], lk_count=1)
+            if tmp[0] > gl_count:
+                gl_count, msg_id = tmp
+    return f"Самый длинный трейд из {gl_count} сообщений начался с сообщения с id - {msg_id}"
+
+
+
 if __name__ == "__main__":
-    print(generate_chat_history())
+    messages = generate_chat_history()
+    print(wrote_the_most_msg(messages))
+    print(most_quoted_user(messages))
+    print(most_readed_user(messages))
+    print(more_popular_time(messages))
+    print(long_trade(messages))
+
+
+
+
+
+
+
+
